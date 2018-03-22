c71be71a645d2c7746068b3892624378592d9865
陈晋添 <chenjintian@cjtdemac-mini.local>
2018-03-22 15:35:04 +0800

    Initial Commit

---
+++ CJTToolAndControls/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJTToolAndControls
+//
+//  Created by 陈晋添 on 2018/3/22.
+//  Copyright © 2018年 cjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CJTToolAndControls/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CJTToolAndControls
+//
+//  Created by 陈晋添 on 2018/3/22.
+//  Copyright © 2018年 cjt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
